1d5eddc225a44f5b5a1de2ed30d6d3b3949fcfc3
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-04-22 01:56:12 +0800

    ⬆️

--- lib/controllers/db.dart
+++ lib/controllers/db.dart
@@ -4,11 +4,14 @@ import 'package:bilibili_subscriber/models/db/uper.dart';
 import 'package:bilibili_subscriber/models/db/video.dart';
 import 'package:get/get.dart';
 import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 class DbService extends GetxService {
   late final Isar isar;
   Future<DbService> init() async {
-    isar = await Isar.open([UperSchema, VideoSchema]);
+    final dir = await getApplicationDocumentsDirectory();
+    // final dataDir = Directory('data').absolute.path;
+    isar = await Isar.open([UperSchema, VideoSchema], directory: dir.path);
     log('Isar inited.', name: 'DbService');
     return this;
   }
